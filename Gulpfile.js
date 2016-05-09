6159ba52560bfeb39cca10353066fe3f16e48dc4
Steven Bell <thestevenbell@gmail.com>
2016-05-09 15:16:27 -0400

    refactored gulpfile and overall file structure to include a dist folder

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,38 +1,16 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade');
     sass = require('gulp-sass');
-    livereload = require('gulp-livereload');
     $ = require('gulp-load-plugins')();
     browserSync = require('browser-sync').create();
 
 var paths = {
-    scss: './stylesheets/*.scss'
+    scss: './dev/stylesheets/*.scss'
 };
 
 //https://www.browsersync.io/docs/gulp/
 // browser-sync set up
-gulp.task('browser-sync', function () {
-   var files = [
-      '/*.html',
-      'stylesheets/css/*.css',
-      '/img/*.png',
-      '/img/*.jpeg',
-      '/img/*.jpg',
-      '/js/*.js'
-   ];
-
-   browserSync.init(files, {
-      server: {
-         baseDir: './'
-      }
-   });
-});
-
 gulp.task('serve', ['styles', 'jade'], function() {
-
-//    browserSync.init({
-//        server: "./"
-//    });
 
   var files = [
       '/*.html',
@@ -40,18 +18,22 @@ gulp.task('serve', ['styles', 'jade'], function() {
       '/img/*.png',
       '/img/*.jpeg',
       '/img/*.jpg',
-      '/js/*.js'
+      '/js/*.js',
+      '/js/users/*.js'
    ];
 
    browserSync.init(files, {
       server: {
-         baseDir: './'
+         baseDir: './dist'
       }
    });
 
     gulp.watch("stylesheets/*.scss", ['styles']);
-    gulp.watch("/*.jade", ['jade']);
-    gulp.watch("/*.html").on('change', browserSync.reload);
+    gulp.watch("dev/html/*.jade", ['jade']);
+    gulp.watch("dist/*.html").on('change', browserSync.reload);
+    gulp.watch("dist/stylesheets/css/*.css").on('change', browserSync.reload);
+    gulp.watch("/js/*.js").on('change', browserSync.reload);
+    gulp.watch("/js/users/*.js").on('change', browserSync.reload);
 });
 
 
@@ -62,26 +44,26 @@ gulp.task('styles', function () {
 
             errLogToConsole: true
         }))
-        .pipe(gulp.dest('./stylesheets/css'))
+        .pipe(gulp.dest('./dist/stylesheets/css'))
 });
 
 // create a TASK to compile Jade to HTML using gulp-jade
 gulp.task('jade', function() {
-   return gulp.src('./*.jade')
+   return gulp.src('./dev/html/*.jade')
    .pipe($.jade({
       pretty: true,
       doctype: 'html'
     }))
    .on('error', $.util.log)
-   .pipe(gulp.dest('./'));
+   .pipe(gulp.dest('./dist'));
 });
 
 // create a TASK to WATCH for changes in your files
 // this will "watch" for any changes in your files and rerun gulp if necessary
+// use the CLI command `gulp watch`
 gulp.task('watch', function() {
-   //livereload.listen();
    gulp.watch(['./*.jade'], ['jade']); //first arg is the dir to watch, 2nd is the task
-   gulp.watch(['./stylesheets/*.scss'], ['styles']);
+   gulp.watch(['./dev/stylesheets/*.scss'], ['styles']);
 });
 
 gulp.task('default',function(){
